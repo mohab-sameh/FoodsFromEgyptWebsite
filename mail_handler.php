cf4fc0c17e548d3139ad8b8bd7e9176194b94acf
Mohab <37941642+whitearsenic@users.noreply.github.com>
2021-10-07 20:04:10 +0200

    Update mail_handler.php

--- mail_handler.php
+++ mail_handler.php
@@ -1,20 +1,12 @@
-<?php 
-if(isset($_POST['email-form'])){
-    $to = "ximatoh105@mxgsby.com"; // this is your Email address
-    $from = $_POST['email']; // this is the sender's Email address
-    $first_name = $_POST['FirstName'];
-    $last_name = $_POST['LastName'];
-    $subject = "Form submission";
-    $subject2 = "Copy of your form submission";
-    $message = $first_name . " " . $last_name . " wrote the following:" . "\n\n" . $_POST['message'];
-    $message2 = "Here is a copy of your message " . $first_name . "\n\n" . $_POST['message'];
+<?php
+$to_email = "xokema9679@cantouri.com";
+$subject = "Simple Email Test via PHP";
+$body = "Hi, This is test email send by PHP Script";
+$headers = "From: sender email";
 
-    $headers = "From:" . $from;
-    $headers2 = "From:" . $to;
-    mail($to,$subject,$message,$headers);
-    mail($from,$subject2,$message2,$headers2); // sends a copy of the message to the sender
-    echo "Mail Sent. Thank you " . $first_name . ", we will contact you shortly.";
-    // You can also use header('Location: thank_you.php'); to redirect to another page.
-    // You cannot use header and echo together. It's one or the other.
-    }
+if (mail($to_email, $subject, $body, $headers)) {
+    echo "Email successfully sent to $to_email...";
+} else {
+    echo "Email sending failed...";
+}
 ?>
